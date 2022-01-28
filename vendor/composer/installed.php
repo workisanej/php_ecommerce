23ccad4a5955f9ac077ac8a21fa5df52822cd51e
Isane Mphela (Intern) <isane.mphela@clickatell.com>
2022-01-28 15:03:10 +0200

    ecommerce commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'edwin/cms',
+  ),
+  'versions' => 
+  array (
+    'edwin/cms' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v5.2.23',
+      'version' => '5.2.23.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7115df4a6f76281109ebe352900c42403b728bb4',
+    ),
+    'pusher/pusher-php-server' => 
+    array (
+      'pretty_version' => 'v3.0.1',
+      'version' => '3.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '03d3417748fc70a889c97271e25e282ff1ff0ae3',
+    ),
+    'vlucas/phpdotenv' => 
+    array (
+      'pretty_version' => 'v2.4.0',
+      'version' => '2.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3cc116adbe4b11be5ec557bf1d24dc5e3a21d18c',
+    ),
+  ),
+);
